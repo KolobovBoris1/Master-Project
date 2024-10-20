be86f724d8182f71f0623e2d000e8e4c55bbfab8
KolobovBoris1 <[kolobovboris1@gmail.com]>
2024-10-20 17:50:00 +0500

    Final Vers

--- disk.sh
+++ disk.sh
@@ -1,12 +1,9 @@
-sleep 30
-df -h > /home/gutteridge/gites/devs/infoCPU.txt
+#!/bin/bash
+df | awk '{NF=NF-1}1' | column -t > /home/gutteridge/gites/devs/infoCPU.txt
 echo "User:" >> /home/gutteridge/gites/devs/infoCPU.txt
 hostname >> /home/gutteridge/gites/devs/infoCPU.txt
 echo "CPU:" >> /home/gutteridge/gites/devs/infoCPU.txt
-lscpu >> /home/gutteridge/gites/devs/infoCPU.txt
-#uname -r
-#cat /proc/version
+uname -snrv >> /home/gutteridge/gites/devs/infoCPU.txt
 echo "Time:" >> /home/gutteridge/gites/devs/infoCPU.txt
 date >> /home/gutteridge/gites/devs/infoCPU.txt
-sleep 10
 
